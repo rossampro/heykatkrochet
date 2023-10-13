65b077e43d878bed34e05520a5354f58f7b19394
kingofdreams777 <rossmartin@sandiego.edu>
2023-10-13 12:22:57 -0700

    Prepping for deploying. Got contact card working and all that good stuff

--- src/components/ImageCard.vue
+++ src/components/ImageCard.vue
@@ -1,5 +1,7 @@
 <script setup>
 
+import ContactCard from './ContactCard.vue';
+
 const plushie = defineProps({
     name: String,
     path: String,
@@ -22,7 +24,15 @@ const imageUrl = getImageUrl(plushie.path);
             <h2 class="card-title">{{ plushie.name }}</h2>
             <p>{{ plushie.description }}</p>
             <div class="card-actions justify-end">
-                <button class="btn btn-primary">Order Now</button>
+                <button class="btn btn-primary" onclick="contactCard.showModal()">Order Now</button>
+                <dialog id="contactCard" class="modal">
+                    <div class="modal-box">
+                        <ContactCard />
+                        <div class="flex flex-col place-items-end">
+                            <button class="btn btn-secondary" onclick="contactCard.close()">Close</button>
+                        </div>
+                    </div>
+                </dialog>
             </div>
             <p v-if="plushie.templateCredit"><i>Crochet Design credits to {{ plushie.templateCredit }}</i></p>
         </div>
--- src/components/WelcomeView.vue
+++ src/components/WelcomeView.vue
@@ -11,7 +11,7 @@ function goToStore() {
         <div class="hero-content flex-col lg:flex-row">
             <img src="@/assets/images/threebees.jpg" class="max-w-sm rounded-lg shadow-2xl" />
             <div>
-                <h1 class="text-5xl font-bold">BoxCat's Crochet &#129526;</h1>
+                <h1 class="text-5xl font-bold">HeyCat Crochet &#129526;</h1>
                 <p class="py-6">Handmade Crochet Stuffed Animals for you or your loved ones</p>
                 <button class="btn btn-primary" @click="goToStore">Visit Store</button>
             </div>
