7ab639ca371020cfb9e3fe230721b20e78fef333
kingofdreams777 <rossmartin@sandiego.edu>
2023-10-24 14:15:50 -0700

    adding dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ export default {
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+        require("daisyui")
+    ],
 }
 
