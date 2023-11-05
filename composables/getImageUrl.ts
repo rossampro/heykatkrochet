c01c921f4b6d0385e00c6005f9a5fd9f6d5fc715
kingofdreams777 <rossmartin@sandiego.edu>
2023-11-05 07:53:30 -0800

    building out update product page

--- composables/getImageUrl.ts
+++ composables/getImageUrl.ts
@@ -1,4 +1,8 @@
-
+/**
+ * Dynamically gets an image url for images within the Assets Directory
+ * @param {string} name
+ * @returns {string}
+ */
 export default function getImageUrl(name: string): string {
     return new URL(`../assets/images/${name}`, import.meta.url).href;
 }
--- pages/admin/updateproduct.vue
+++ pages/admin/updateproduct.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import type { IPlushie } from '~/models/plushie';
+
+const { data, pending, error } = await
+    useLazyAsyncData<IPlushie[]>('/api/products', () => {
+        return $fetch('/api/products')
+    })
+</script>
 <template>
     <div>
 
