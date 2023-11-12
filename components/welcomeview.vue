95cdcce710464b621e9af59b818ae3a779e6f1aa
kingofdreams777 <rossmartin@sandiego.edu>
2023-11-11 21:55:18 -0800

    renaming admin login functionality

--- components/welcomeview.vue
+++ components/welcomeview.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="hero min-h-screen bg-base-200">
         <div class="hero-content flex-col lg:flex-row">
-            <NuxtImg format="webp" src="/threebees.jpg" class="max-w-sm rounded-lg shadow-2xl" />
+            <LazyNuxtPicture format="webp" src="/threebees.jpg" class="max-w-sm rounded-lg shadow-2xl" />
             <div class="prose">
                 <h1 class="text-5xl font-bold">HeyKat Krochet &#129526;</h1>
                 <p class="py-6">Handmade Crochet Stuffed Animals for you or your loved ones</p>
